227337e939701163321822e8becc9450ae69b911
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2020-09-04 19:52:02 +0900

    CREATE:06~10.swift

---
+++ Learn to Code 1/06~10.swift
@@ -0,0 +1,95 @@
+//
+//  06~10.swift
+//  Swift_PrayGround
+//
+//  Created by Glory on 2020/09/04/. 19:50
+//  Copyright © 2020 Glory. All rights reserved.
+//
+
+//06
+//Bug Squash Practice
+moveForward()
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+toggleSwitch()
+turnLeft()
+turnLeft()
+turnLeft()
+moveForward()
+moveForward()
+moveForward()
+moveForward()
+collectGem()
+
+//07
+//The Shortest Route
+moveForward()
+moveForward()
+moveForward()
+collectGem()
+moveForward()
+moveForward()
+moveForward()
+moveForward()
+toggleSwitch()
+
+//Function
+//08
+//Composing a New Behavior
+moveForward()
+moveForward()
+moveForward()
+turnLeft()
+turnLeft()
+turnLeft()
+moveForward()
+moveForward()
+moveForward()
+collectGem()
+
+//09
+//Creating a New Function
+func turnRight(){
+    turnLeft()
+    turnLeft()
+    turnLeft()
+}
+moveForward()
+turnLeft()
+moveForward()
+turnRight()
+moveForward()
+turnRight()
+moveForward()
+turnRight()
+moveForward()
+turnLeft()
+moveForward()
+toggleSwitch()
+
+//10
+//Collect, Toggle, Repeat
+func func_Repeat(){
+    moveForward()
+    collectGem()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    turnLeft()
+    moveForward()
+    collectGem()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    moveForward()
+    turnLeft()
+}
+
+func_Repeat()
+func_Repeat()
