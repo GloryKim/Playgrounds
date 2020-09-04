9ac0f416bd336814ece53770dcd41f89dbe751b5
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2020-09-04 19:01:41 +0900

    CREATE:01-05.swift

---
+++ Learn to Code 1/01~05.swift
@@ -0,0 +1,73 @@
+//
+//  01~05.swift
+//  Swift_PrayGround
+//
+//  Created by Glory on 2020/09/04/. 18:38
+//  Copyright © 2020 Glory. All rights reserved.
+//
+
+import Foundation
+print("Hello, World!")
+
+//01
+//Issuing Commands
+moveForward()
+moveForward()
+moveForward()
+collectGem()
+
+//02
+//Adding a New Command
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+collectGem()
+
+//03
+//Toggling a Switch
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+collectGem()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+toggleSwitch()
+
+//04
+//Portal Practice
+moveForward()
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+toggleSwitch()
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+collectGem()
+
+//05
+//Finding and Fixing Bugs
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+collectGem()
+moveForward()
+toggleSwitch()
+//change
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+collectGem()
+moveForward()
+toggleSwitch()
---
+++ Learn to Code 1/XX~XX.swift
@@ -0,0 +1,10 @@
+//
+//  xx~xx.swift
+//  Swift_PrayGround
+//
+//  Created by Glory on 2020/xx/xx/. xx:xx
+//  Copyright © 2020 Glory. All rights reserved.
+//
+
+//xx
+//Sample
