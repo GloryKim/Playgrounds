f8c7e148eff51442d36d04ccba5df74a02b02e11
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2020-10-23 10:05:07 +0900

    Runnig Code While

---
+++ Learn to Code 1/Running Code While.swift
@@ -0,0 +1,12 @@
+//
+//  Running Code While.swift
+//  Swift_PrayGround
+//
+//  Created by Glory on 2020/10/23/
+//  Copyright © 2020 Glory. All rights reserved.
+//
+
+while isOnClosedSwitch {// 반복문
+toggleSwitch()
+moveForward()
+}
